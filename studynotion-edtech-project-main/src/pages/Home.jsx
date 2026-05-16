42fb7ccfba4ddc0d1778ba9f1ba58e5c89ced923
Abishek1254 <abisheksalaria11012005@gmail.com>
2026-05-16 19:02:33 +0530

    Finding potential issues in deploymnet

--- studynotion-edtech-project-main/src/pages/Home.jsx
+++ studynotion-edtech-project-main/src/pages/Home.jsx
@@ -38,7 +38,7 @@ function Home() {
 
         {/* Sub Heading */}
         <div className="-mt-3 w-[90%] text-center text-lg font-bold text-richblack-300">
-          With our online coding courses, you can learn at your own pace, from
+          With our online coding courses, you can learn at your own specific pace, from
           anywhere in the world, and get access to a wealth of resources,
           including hands-on projects, quizzes, and personalized feedback from
           instructors.
