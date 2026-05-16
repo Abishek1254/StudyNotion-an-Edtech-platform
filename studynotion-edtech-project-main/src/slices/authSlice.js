d2b565769acc91ba262c89fc1d427234467ee1c9
Abishek1254 <abisheksalaria11012005@gmail.com>
2026-05-16 16:33:23 +0530

    Dummy Commit changes to check working

--- studynotion-edtech-project-main/src/slices/authSlice.js
+++ studynotion-edtech-project-main/src/slices/authSlice.js
@@ -8,7 +8,7 @@ const initialState = {
 
 const authSlice = createSlice({
   name: "auth",
-  initialState: initialState,
+  initialState: initialState, 
   reducers: {
     setSignupData(state, value) {
       state.signupData = value.payload;
